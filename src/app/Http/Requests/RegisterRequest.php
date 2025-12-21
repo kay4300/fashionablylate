42921a8b0466748c5315826cc2a5980219f35c33
kay4300 <alltoowell22red@gmail.com>
2025-12-22 02:27:14 +0900

    2025.12.21更新再

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -27,7 +27,6 @@ class RegisterRequest extends FormRequest
             'name'     => ['required', 'string', 'max:255'],
             'email'    => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'string', 'min:8'],
-            'category_id' => ['required', 'exists:categories,id'],
         ];
     }
 
